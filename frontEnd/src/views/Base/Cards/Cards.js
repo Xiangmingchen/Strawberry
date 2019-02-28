0861806011e61004ed89640bb1ecdbef33aad2da
Jenny Chen <xc14@illinois.edu>
2019-02-27 21:40:58 -0600

    Changed cards page to product page

--- frontEnd/src/views/Base/Cards/Cards.js
+++ frontEnd/src/views/Base/Cards/Cards.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import { Badge, Card, CardBody, CardFooter, CardHeader, Col, Row, Collapse, Fade } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { Badge, Card, CardBody, CardImg, CardTitle, CardText, CardFooter, CardHeader, Col, Row, Collapse, Fade } from 'reactstrap';
 import { AppSwitch } from '@coreui/react'
+import sampleImg from '../../../assets/img/products/sample.jpg'
+import sampleImg2 from '../../../assets/img/products/sample2.jpg'
+import sampleImg3 from '../../../assets/img/products/sample3.jpeg'
 
 class Cards extends Component {
   constructor(props) {
@@ -23,44 +27,75 @@ class Cards extends Component {
     this.setState((prevState) => { return { fadeIn: !prevState }});
   }
 
+  /**
+   * Shorten the product name down to a certain number of words
+   * if the title is too long, take out the last few words and add "..." at
+   * the end
+   * @param {string} productName the name of product
+   */
+  shortenProductName(productName) {
+    const threshold = 10
+    const words = productName.trim().split(" ")
+    if (words.length > threshold) {
+      return words.slice(0, threshold).join(" ") + "..."
+    }
+    return productName
+  }
+
+  preprocessProductImg(image) {
+    console.log(image)
+    return image
+  }
+
   render() {
     return (
       <div className="animated fadeIn">
         <Row>
           <Col xs="12" sm="6" md="4">
             <Card>
-              <CardHeader>
-                Card title
-              </CardHeader>
+              <CardImg top className="product_img" src={this.preprocessProductImg(sampleImg)} alt="Product Image"/>
               <CardBody>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-                laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-                ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                <CardTitle>
+                 <Link to="/products/product_number">
+                  <b>{this.shortenProductName("Sample Product Name That Is Fetched From API")}</b>
+                 </Link>
+                </CardTitle>
+                <CardText>
+                  Lowest past price: $20<br/>
+                  Current price: $30
+                </CardText>
               </CardBody>
             </Card>
           </Col>
           <Col xs="12" sm="6" md="4">
-            <Card>
-              <CardBody>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-                laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-                ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-              </CardBody>
-              <CardFooter>Card footer</CardFooter>
-            </Card>
+          <Card>
+            <CardImg top className="product_img" src={sampleImg2} alt="Product Image"/>
+            <CardBody>
+              <CardTitle>
+               <Link to="/products/product_number">
+               <b>{this.shortenProductName("If the product name is too long, it will be shortened")}</b>
+               </Link>
+              </CardTitle>
+              <CardText>
+                Lowest past price: $29.5<br/>
+                Current price: $40
+              </CardText>
+            </CardBody>
+          </Card>
           </Col>
           <Col xs="12" sm="6" md="4">
             <Card>
-              <CardHeader>
-                Card with icon
-                <div className="card-header-actions">
-                  <i className="fa fa-check float-right"></i>
-                </div>
-              </CardHeader>
+              <CardImg top className="product_img" src={sampleImg3} alt="Product Image"/>
               <CardBody>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-                laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-                ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+                <CardTitle>
+                 <Link to="/products/product_number">
+                 <b>{this.shortenProductName("What if the picture is vertically longer")}</b>
+                 </Link>
+                </CardTitle>
+                <CardText>
+                  Lowest past price: $20<br/>
+                  Current price: $30
+                </CardText>
               </CardBody>
             </Card>
           </Col>
